4f208a61c3fc9391452b38b341518323a255b3f4
nathanguk <nathan.gaskill@ansgroup.co.uk>
2018-03-01 17:23:13 +0000

    Fixed offset issue

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var azure = require('azure-storage');
 var multiparty = require('multiparty');
 var uuidv1 = require('uuid/v1');
 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
@@ -13,53 +15,52 @@ app.get('/', function(req, res){
 });
 
 app.post('/upload', function (req, res) {
+
+  var uri;
+  var name;
   var blobService = azure.createBlobService();
   var form = new multiparty.Form();
   form.on('part', function(part) {
-      if (part.filename) {
-          var size = part.byteCount - part.byteOffset;
-          var extension = part.filename.split(".")[1].toLowerCase();
-          //if(extension == "jpg"){ extension = "jpeg" };
-          contentType = "image/"+extension;
-          var options = {contentSettings:{contentType:contentType}}
 
-          var name = uuidv1() + "." + extension;
+    part.on('error', function(error) {
+        console.log(error);
+        console.log("Part Error");
+        res.send(error);
+    });
+    
+    if (part.filename) {
+        var size = part.byteCount; // - part.byteOffset;
+        var extension = part.filename.split(".")[1].toLowerCase();
+        if(extension == "jpg"){ extension = "jpeg" };
+        var contentType = "image/"+extension;
+        var options = {contentSettings:{contentType:contentType}}
 
-              blobService.createBlockBlobFromStream('images', name, part, size, options, function(error, result, response) {
-                    if (error) {
-                        console.log(error);
-                        res.send(error);
-                    }else{
-                          // Create a SAS token that expires in 24 hours
-                          // Set start time to five minutes ago to avoid clock skew.
-                          var startDate = new Date();
-                          startDate.setMinutes(startDate.getMinutes() - 5);
-                          var expiryDate = new Date(startDate);
-                          expiryDate.setMinutes(startDate.getMinutes() + 1440); //24 Hours from now
-      
-                          var permissions = permissions || azure.BlobUtilities.SharedAccessPermissions.READ;
-      
-                          var sharedAccessPolicy = {
-                              AccessPolicy: {
-                                  Permissions: permissions,
-                                  Start: startDate,
-                                  Expiry: expiryDate
-                              }
-                          };
-                          
-                          var sasToken = blobService.generateSharedAccessSignature(result.container, result.name, sharedAccessPolicy);
-                          var uri = blobService.getUrl(result.container, result.name, sasToken, true);
-      
-                          res.send(uri);
-                    };
-                });
-   
-      } else {
-          form.handlePart(part);
-      }
+        name = uuidv1() + "." + extension;
+
+        blobService.createBlockBlobFromStream('images', name, part, size, options, function(error, result, response) {
+            if (error) {
+                console.log("Blob Error");
+                console.log(error);
+                res.send(error);
+            }else{
+                console.log(result)
+            }
+        });
+    };
   });
+
+
+  form.on('close', function() {
+    storageAccount = process.env.AZURE_STORAGE_ACCOUNT;
+    uri = "https://"+storageAccount+".blob.core.windows.net/images/"+name;
+    function sendUri(){
+        res.send(uri);
+    };
+    setTimeout(sendUri, 500);
+  }); 
+
+  
   form.parse(req);
-  res.send('OK');
 });
 
 module.exports = app;
